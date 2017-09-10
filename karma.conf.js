df9eb43a922a3ec743aebec03685f0dbb9c03a53
nagakarthikeyan <naga.karthikeyan@neustar.biz>
2017-09-10 20:26:38 +0530

    Karma configration update

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-    reporters: ['spec',' junit'],
+    reporters: ['spec'],
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
